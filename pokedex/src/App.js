448e1701f86f13ce52e71797e3153ac65bdf1f49
daviskyLV <ledaviskylv@gmail.com>
2023-04-24 00:24:32 +0200

    hashrouter test

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,24 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { getPokemonData, getPokemons } from "./components/Datafetch";
 import About from "./components/About";
 import "./App.css";
 import Home from './components/Homepage';
 import Header from "./components/Header";
 import PokemonDetail from './components/pokemon_details';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Header/>
         <Routes>
           <Route path ="about" element={<About />} exact />
           <Route path="/detail/:id" element={<PokemonDetail/>}/>
           <Route path="*" element={<Home />} exact />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
