a4efe43426551bf95d6122317739af77a94eaa6f
daviskyLV <ledaviskylv@gmail.com>
2023-04-24 00:41:28 +0200

    hashrouter again

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -4,20 +4,20 @@ import "./App.css";
 import Home from './components/Homepage';
 import Header from "./components/Header";
 import PokemonDetail from './components/pokemon_details';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Header/>
         <Routes>
           <Route path ="about" element={<About />} exact />
           <Route path="/detail/:id" element={<PokemonDetail/>}/>
           <Route path="*" element={<Home />} exact />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
