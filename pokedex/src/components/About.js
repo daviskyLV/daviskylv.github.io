2d572c105ad9ffa206e5f814c705fcdadfc6021b
daviskyLV <ledaviskylv@gmail.com>
2023-04-24 00:47:18 +0200

    credits

--- pokedex/src/components/About.js
+++ pokedex/src/components/About.js
@@ -5,7 +5,11 @@ function About() {
     return (
         <div className="about">
             <h1>About</h1>
-            <p>This is the Pokédex assignment for WEB2.</p>
+            <p>This is the Pokédex assignment for WEB2. Made by
+                <a href="https://github.com/EmiliaNistor">Emilia</a>,
+                <a href="https://github.com/HugoSilva4">Hugo</a> and
+                <a href="https://github.com/daviskyLV">Andrejs</a>
+            </p>
             <p>Pokémon are creatures of all shapes and sizes who live in the 
                 wild or alongside their Trainers. During their adventures, 
                 Pokémon grow and become more experienced and even, 
